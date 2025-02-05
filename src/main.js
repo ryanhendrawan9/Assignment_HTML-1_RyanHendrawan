be8b0dad8b9efbf0748d0e7540283901cdd845b0
Ryan Hendrawan <ryanhendrawan9@gmail.com>
2025-02-05 22:47:21 +0800

    feat : Footer & Scroll Up + (JS)

--- src/main.js
+++ src/main.js
@@ -32,3 +32,44 @@ const scrollHeader = () => {
 };
 
 window.addEventListener("scroll", scrollHeader);
+
+// Show Scroll Up
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// Active Link
+const activeLink = () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  let current = "home";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((item) => {
+    item.classList.remove("active");
+
+    if (item.href.includes(current)) {
+      item.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", activeLink);
